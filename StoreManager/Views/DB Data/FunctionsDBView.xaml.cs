4345cdaa2c9d627959d1b110dd959234ccd9064c
Dmytrii7V <114422312+Dmytrii7V@users.noreply.github.com>
2023-12-25 19:49:24 +0100

    Add views

---
+++ StoreManager/Views/DB Data/FunctionsDBView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StoreManager.Views.DB_Data
+{
+    /// <summary>
+    /// Interaction logic for FunctionsDBView.xaml
+    /// </summary>
+    public partial class FunctionsDBView : Window
+    {
+        public FunctionsDBView()
+        {
+            InitializeComponent();
+        }
+    }
+}
