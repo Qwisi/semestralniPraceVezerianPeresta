4bd08e2868062e17940be4444be5627d392c3a6d
Qwisi <peresta.vania@gmail.com>
2023-12-27 00:31:18 +0100

    Add view for Admin

---
+++ StoreManager/Views/Admin/ViewsInfo/ViewInventoryView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StoreManager.Views.Admin.ViewsInfo
+{
+    /// <summary>
+    /// Interaction logic for ViewInventoryView.xaml
+    /// </summary>
+    public partial class ViewInventoryView : Window
+    {
+        public ViewInventoryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ StoreManager/Views/Admin/ViewsInfo/ViewSupplierView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StoreManager.Views.Admin.ViewsInfo
+{
+    /// <summary>
+    /// Interaction logic for ViewSupplierView.xaml
+    /// </summary>
+    public partial class ViewSupplierView : Window
+    {
+        public ViewSupplierView()
+        {
+            InitializeComponent();
+        }
+    }
+}
